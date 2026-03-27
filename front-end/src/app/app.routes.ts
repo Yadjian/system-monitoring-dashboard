3eb5e97c92f8825642b557171946bd29dbf79fca
Lucas-Boyadjian <10462@holbertonstudents.com>
2026-03-27 18:35:45 +0100

    feat: implement dashboard component with system metrics display and styling

--- front-end/src/app/app.routes.ts
+++ front-end/src/app/app.routes.ts
@@ -1,6 +1,6 @@
 import { Routes } from '@angular/router';
-import { Home } from './pages/home/home';
+import { Dashboard } from './components/dashboard/dashboard';
 
 export const routes: Routes = [
-  { path: '', component: Home },
+  { path: '', component: Dashboard },
 ];
--- front-end/src/app/components/dashboard/dashboard.ts
+++ front-end/src/app/components/dashboard/dashboard.ts
@@ -1,21 +1,25 @@
 import { Component, OnInit } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { CommonModule, DecimalPipe } from '@angular/common';
+import { ChangeDetectorRef } from '@angular/core';
 
 @Component({
-  selector: 'app-home',
+  selector: 'app-dashboard',
   imports: [CommonModule, DecimalPipe],
-  templateUrl: './home.html',
-  styleUrl: './home.css',
+  templateUrl: './dashboard.html',
+  styleUrl: './dashboard.css',
 })
-export class Home implements OnInit {
+export class Dashboard implements OnInit {
   metrics: any;
 
-  constructor(private http: HttpClient) {}
+  constructor(private http: HttpClient, private cdr: ChangeDetectorRef) {}
 
   ngOnInit() {
     this.http.get('/api/monitoring').subscribe({
-      next: data => this.metrics = data,
+      next: data => {
+        this.metrics = data;
+        this.cdr.detectChanges();
+      },
       error: err => console.error('Erreur lors du chargement des métriques', err)
     });
   }
